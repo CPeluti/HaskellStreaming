b595f070baf87da36372fb24be5d0109362860f5
Caio Peluti <cpeluti@gmail.com>
2023-12-11 20:21:01 -0300

    audio fununcia!!!!

--- app/RestApi.hs
+++ app/RestApi.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module RestApi where
 
-import Data.Foldable (for_)
+import           Data.Foldable (for_)
+import           Web.Scotty as Scotty
+import           Text.Blaze.Html.Renderer.Text (renderHtml)
+import           Text.Blaze.Html5 as H
+import           Text.Blaze.Html5.Attributes as A
+import           Network.HTTP.Types
+import           Control.Monad.Trans.Resource
+import           Network.Wai
+import           Streaming                   
+import qualified Streaming.Prelude               as S
+import           Data.ByteString.Builder (byteString, Builder)
+import           Data.ByteString as B
+import           Data.ByteString.Streaming  as BSS (toChunks, readFile)
 
-import Web.Scotty as Scotty
-import Text.Blaze.Html.Renderer.Text (renderHtml)
-import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as A
 
 hxPost content = customAttribute "hx-post" content
 hxSwap content = customAttribute "hx-swap" content
@@ -16,6 +24,23 @@ myButton = button ! hxPost "/clicked" ! hxSwap "outerHTML" $ "Click me"
 componentButton id = button $ id
 
 dbData = [1 .. 4]
+
+fileSize f = do
+  file <- B.readFile f
+  return $ B.length file
+
+generateStream f = BSS.toChunks $ BSS.readFile f 
+
+streamingBD :: Stream (Of ByteString) (ResourceT IO) r -> StreamingBody
+streamingBD stream = 
+  streamingBody 
+  where
+  streamingBody writeBuilder flush =
+    let writer a = 
+            do liftIO (writeBuilder (byteString a))
+              -- flushes for every produced bytestring, perhaps not optimal
+               liftIO flush
+     in runResourceT $ void $ S.effects $ S.for stream writer
 
 restApi :: IO ()
 restApi = 
@@ -32,3 +57,7 @@ restApi =
         H.div $
           for_ (Prelude.map show dbData) $ \(id) ->
             componentButton $ toHtml id
+    get "/music" $ do
+      Scotty.setHeader "Content-Type" "audio/mpeg"
+      Scotty.setHeader "Content-Length" "5827011"
+      Scotty.stream $ streamingBD $ generateStream "/home/caio/projetos/unb/haskell/projeto-final-grupo-2/app/eBG7P-K-r1Y_160.mp3"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,9 @@
 module Lib
     ( someFunc
     ) where
-import qualified Data.ByteString as B
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-teste f = do 
-    res <- B.readFile f
-    print res
+
+
