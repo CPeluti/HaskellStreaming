0fa38affd1321df738e1eac96a0d631a9543d09f
luccahuguet <lucca.huguet2@gmail.com>
2023-12-18 00:22:49 -0300

    makes views modular

--- app/Views.hs
+++ app/Views.hs
@@ -12,23 +12,35 @@ loginPage = [hsx|
         <body class="bg-gray-900 text-white font-sans flex justify-center items-center h-screen m-0">
             <div class="bg-gray-800 p-10 rounded-lg shadow-lg w-72">
                 <h1 class="text-4xl mb-5 text-center">Login</h1>
-                <form class="space-y-5" method="post" action="/login">
-                    <div>
-                        <input class="bg-gray-700 w-full p-4 rounded text-white focus:outline-none focus:shadow-outline" 
-                               type="text" id="username" name="username" placeholder="Username" required>
-                    </div>
-                    <div>
-                        <input class="bg-gray-700 w-full p-4 rounded text-white mb-4 focus:outline-none focus:shadow-outline" 
-                               type="password" id="password" name="password" placeholder="Password" required>
-                    </div>
-                    <div>
-                        <button class="w-full p-4 bg-purple-500 text-white rounded focus:outline-none focus:shadow-outline hover:bg-green-600 transition-colors" 
-                                type="submit">
-                            Login
-                        </button>
-                    </div>
-                </form>
+                {loginForm}
             </div>
         </body>
     </html>
+|]
+
+loginForm :: Html.Html
+loginForm = [hsx|
+    <form class="space-y-5" method="post" action="/login">
+        {inputField "text" "username" "Username"}
+        {inputField "password" "password" "Password"}
+        {loginButton}
+    </form>
+|]
+
+inputField :: String -> String -> String -> Html.Html
+inputField fieldType name placeholder = [hsx|
+    <div>
+        <input class="bg-gray-700 w-full p-4 rounded text-white focus:outline-none focus:shadow-outline" 
+               type={fieldType} id={name} name={name} placeholder={placeholder} required>
+    </div>
+|]
+
+loginButton :: Html.Html
+loginButton = [hsx|
+    <div>
+        <button class="w-full p-4 bg-purple-500 text-white rounded focus:outline-none focus:shadow-outline hover:bg-green-600 transition-colors" 
+                type="submit">
+            Login
+        </button>
+    </div>
 |]
